0c6f20fd9a6e58caa6569487f572066f3ae63a27
Louis <lpil@users.noreply.github.com>
2014-05-05 17:00:10 +0100

    hw0 p2 remove test

--- hw0/hw0_part2.rb
+++ hw0/hw0_part2.rb
@@ -6,8 +6,6 @@ def starts_with_consonant?(s)
   !! (s =~ /^[BCDFGHJKLMNPQRSTVXZWY]/i)
 end
 
-p starts_with_consonant?("D$(@#*&^$(&@^(@^*$")
-
 def binary_multiple_of_4?(s)
   return false unless s.match /^-?[01]+$/
   s.to_i(2) % 4 == 0
